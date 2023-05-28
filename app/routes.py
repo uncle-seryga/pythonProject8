d42abe68a6eb7e44ac3e48f59a944ff2f26ef6d9
serhiialpatov <alpatovsn@gmail.com>
2023-05-28 14:32:59 +0300

    excange logic almost done 1#todo left

--- app/routes.py
+++ app/routes.py
@@ -17,11 +17,15 @@ def index():
 @app.route("/")
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    form = forms.Currency()
-    if form.is_submitted():
-        print(flask.request.form.get("currencies-list-1"))
-        print(flask.request.form.get("currencies-list-2"))
     list_of_currency_names = [x for x in currency.set_currency()]
     list_of_currency_names_full = currency.set_currency()
+    form = forms.Currency()
+    if form.is_submitted():
+        a = flask.request.form.get("currencies-list-1")
+        b = flask.request.form.get("currencies-list-2")
+        print(form.submit.data)  # todo return False, input data expected -> research
+        result = currency.convert(a, b, form.submit.data)
+        return flask.render_template("currency_main.html", currency_list=list_of_currency_names_full,
+                                     input_amount=form.search, result=0, form=form)
     return flask.render_template("currency_main.html", currency_list=list_of_currency_names_full,
                                  input_amount=form.search, result=0, form=form)
--- currency.py
+++ currency.py
@@ -1,4 +1,5 @@
 import requests
+import codecs
 
 
 def download_currency():
@@ -12,14 +13,14 @@ def download_currency():
 
 
 def save_currency_json(data):
-    with open('currency/data.json', 'w') as f:
+    with codecs.open('currency/data.json', 'w', 'utf-8') as f:
         f.write(str(data))
 
 
 def set_currency():
     exceptions = []
     results = {}
-    with open('currency/countries.json','r') as counties:
+    with open('currency/countries.json', 'r') as counties:
         counties = eval(counties.read())
     with open('currency/data.json', 'r') as cur:
         cur_data = eval(cur.read())
@@ -32,20 +33,50 @@ def set_currency():
                             for z in counties:
                                 try:
                                     if y["code"] in z['currencies'] and y['code'] not in exceptions:
-                                        results.update({y["code"]: [x['rateCross'], y['name'],z['cca2']]})
+                                        results.update({y["code"]: [x['rateCross'], y['name'], z['cca2']]})
                                         exceptions.append(y['code'])
                                         break
                                 except KeyError:
-                                        results.update({y["code"]: [x['rateCross'], y['name'], z['cca2']]})
-                                        break
+                                    results.update({y["code"]: [x['rateCross'], y['name'], z['cca2']]})
+                                    break
                 elif x['currencyCodeB'] != 840:
                     for y in iso_data:
                         if y['number'] == str(x['currencyCodeA']):
                             for z in counties:
                                 if y["code"] in z['currencies'] and y['code'] not in exceptions:
-                                    results.update({y["code"]: [x['rateBuy'], y['name'] ,z['cca2']]})
+                                    results.update({y["code"]: [x['rateBuy'], y['name'], z['cca2']]})
                                     exceptions.append(y['code'])
                                     break
     return results
 
-# print(set_currency())
+
+def convert(cur1, cur2, amount):
+    iso = eval(open('currency/iso4217.json', 'r').read())
+    for x in iso:
+        if x["code"] == cur1:
+            cur1_code = int(x["number"])
+    else:
+        cur1_code = 980
+    for x in iso:
+        if x["code"] == cur2:
+            cur2_code = int(x["number"])
+    else:
+        cur2_code = 980
+    data = eval(open('currency/data.json', 'r').read())
+    for x in data:
+        if x["currencyCodeA"] == cur1_code:
+            if x['rateBuy'] == 0:
+                cur1_price = x['rateCross']
+            else:
+                cur1_price = x['rateBuy']
+    else:
+        cur1_price = 1
+    for x in data:
+        if x["currencyCodeA"] == cur2_code:
+            if x['rateBuy'] == 0:
+                cur2_price = x['rateCross']
+            else:
+                cur2_price = x['rateBuy']
+    else:
+        cur2_price = 1
+    return (cur2_price / cur1_price * amount)
